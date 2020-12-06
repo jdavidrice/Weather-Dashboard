c5dc1a0aa112908bd5ec219912af67abce09820e
Jeremy Rice <jdavidrice@gmail.com>
2020-12-06 18:52:25 -0500

    testing css links

---
+++ assets/app.js
@@ -0,0 +1,14 @@
+// To make API call
+
+// api.openweathermap.org/data/2.5/forecast?id=524901&appid={API key}
+
+function buildQueryURL() {
+
+  const myApiKey = { 'api-key': '857d8b3aed4b93541a08ad2a027ffb0c' };
+
+
+
+
+
+}
+

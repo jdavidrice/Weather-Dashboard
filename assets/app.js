fff6b82e44d5be6c7443478bf81935fc5501711b
Jeremy Rice <jdavidrice@gmail.com>
2020-12-06 23:05:59 -0500

    added API call, fetch weather, and change location JS

--- assets/app.js
+++ assets/app.js
@@ -1,14 +1,37 @@
 // To make API call
 
-// api.openweathermap.org/data/2.5/forecast?id=524901&appid={API key}
+class Weather {
+  constructor(city) {
+    this.apiKey = '857d8b3aed4b93541a08ad2a027ffb0c';
+    this.city = city;
+    // this.state = state;
+  }
 
-function buildQueryURL() {
+  // Fetch weather from API
+  async getWeather() {
+    const response = await fetch('http://api.openweathermap.org/data/2.5/forecast?q=Boston&appid=857d8b3aed4b93541a08ad2a027ffb0c');
 
-  const myApiKey = { 'api-key': '857d8b3aed4b93541a08ad2a027ffb0c' };
+    const responseData = await response.json();
 
+    return responseData.list;
+  }
 
-
-
-
+  // Change weather location
+  changeLocation(city) {
+    this.city = city;
+    // this.state = state;
+  }
 }
+
+
+// Init weather object
+const weather = new Weather('Boston');
+
+weather.getWeather()
+  .then(results => {
+    console.log(results);
+  })
+  .catch(err => console.log(err));
+
+
 

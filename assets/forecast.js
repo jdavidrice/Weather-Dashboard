0ae6b8b54cf1f51ccd5433abe7fdd51d8bfe0f56
Jeremy Rice <jdavidrice@gmail.com>
2020-12-12 22:46:58 -0500

    really add forecast.js

---
+++ assets/forecast.js
@@ -0,0 +1,26 @@
+class Forecast {
+  constructor(city) {
+    this.apiKey = '857d8b3aed4b93541a08ad2a027ffb0c';
+    this.city = city;
+  }
+
+
+  // Fetch forecast from API
+  async getForecast() {
+    const forecastResponse = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${this.city}&appid=${this.apiKey}&units=imperial`);
+
+    const forecastResponseData = await forecastResponse.json();
+
+    console.trace(forecastResponseData)
+    return forecastResponseData;
+
+  }
+
+
+  // Change weather location
+  // changeLocation(city) {
+  //   this.city = city;
+  // }
+
+  // The curly bracket below closes the Weather class from the top of the page
+}  
